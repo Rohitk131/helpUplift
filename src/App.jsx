845a65f69e8ff11b45f63b29386e27b46466ce22
rohitk131 <rohitkumar13103@gmail.com>
2024-02-03 17:59:42 +0530

    Merge conflicts resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 
 export default function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-        <Routes>
-          <Route path="/login" element={<Login/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }

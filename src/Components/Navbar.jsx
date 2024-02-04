779ec8e4e9c62ca340a6d126ab2751012debc108
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-02-04 20:16:37 +0530

    logo included

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Logo from '/logo.png'
+import {Link} from 'react-router-dom'
 
 const navigation = [
   { name: 'Home', href: '/', current: window.location.pathname == "/" ? true : false },
@@ -21,6 +23,7 @@ export default function Example() {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
+            <Link to='/'><img className='h-10 pr-10' src={Logo} alt='HelpUplift'/></Link>
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
